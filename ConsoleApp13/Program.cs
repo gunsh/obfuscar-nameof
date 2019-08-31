12aecc0c1bc49506a64aea86e44e2e8e042a75da
Aleksandar Puskas <apuskas@gunsh.com>
2019-08-31 09:05:26 +0200

    Import

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace ConsoleApp13
+{
+    public class Program
+    {
+        [XmlIgnore] public string MyProperty { get; set; }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine($"nameof(MyProperty) = {nameof(MyProperty)}");
+            Console.WriteLine($"MyProperty = {typeof(Program).GetProperties()[0].Name}");
+        }
+    }
+}
